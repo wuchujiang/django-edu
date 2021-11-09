42fde1e6ea4740b1a512bcae8a7ca59dd9e3c012
wcj <chujiangwu@oakblack.com>
2021-11-09 17:50:27 +0800

    init

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -5,6 +5,9 @@ from django.shortcuts import render
 from django.views.generic.base import View
 from pure_pagination import Paginator, PageNotAnInteger
 
+from .operation.models import UserCourse
+from .utils.mixin_utils import LoginRequiredMixin
+from .operation.models import UserFavorite
 from .models import Course
 
 
@@ -43,3 +46,53 @@ class CourseListView(View):
             "sort": sort,
             "hot_courses": hot_course
         })
+
+
+class CourseDetailView(View):
+    """
+    课程详情页
+    """
+    def get(self, request, course_id):
+        course = Course.objects.get(id=int(course_id))
+
+        # 增加课程点击数
+        course.click_nums +=1
+        course.save()
+
+        # 是否收藏课程
+        has_fav_course = False
+        has_fav_org = False
+
+        if request.use.is_authenticated():
+            if UserFavorite.objects.filter(user=request.user, fav_id=course.id, fav_type=1):
+                has_fav_course = True
+
+            if UserFavorite.objects.filter(user=request.user, fav_id=course.course_org.id,fav_type=2):
+                has_fav_org = True
+
+        tag = course.tag
+
+        if tag:
+            relate_course = Course.objects.filter(tag=tag)[:1]
+        else:
+            relate_course = []
+
+        return render(request, "course-detail.html", {
+            "course": course,
+            "relate_coures": relate_course,
+            "has_fav_course": has_fav_course,
+            "has_fav_org": has_fav_org
+        })
+
+
+class CourseInfoView(LoginRequiredMixin,View):
+    """
+    课程章节信息
+    """
+    def get(self, request, course_id):
+        course = Course.objects.get(id=int(course_id))
+        course.students +=1
+        course.save()
+
+        # 查询用户是否已经关联了该课程
+        user_courses = UserCourse.objects
